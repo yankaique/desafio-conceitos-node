80db53daec158b95ce84bfb6c6120a85574e49f5
Yan <yankaiquecosta.yk@gmail.com>
2020-10-29 05:57:20 -0300

    Criando requisições

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+
+  const items = {id:uuid(),title,url,techs,likes:0}
+  repositories.push(items)
+  return response.json(items);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title,url,techs} = request.body;
+
+  const itemIndex = repositories.findIndex(item=>item.id === id);
+
+  if(itemIndex<0){
+    return response.status(400).json("Repositories not found.")
+  } 
+
+  if(itemIndex === -1){
+    return response.status(400).json({error: 'Repository does not exist'});
+  }
+
+  const items = {id,title,url,techs,likes:repositories[itemIndex].likes}
+
+  repositories[itemIndex] = items;
+
+  return response.json(items);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const itemIndex = repositories.findIndex(item=>item.id === id);
+
+  if(itemIndex<0){
+    return response.status(400).json("Repositories not found.")
+  }
+
+  repositories.splice(itemIndex,1);
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const itemIndex = repositories.findIndex(item=>item.id===id); 
+
+  if(itemIndex<0){
+    return response.status(400).json("Repositories not found.")
+  };
+
+  repositories[itemIndex].likes++
+
+
+  return response.json(repositories[itemIndex]);
 });
 
 module.exports = app;
